287fae9c6c7231776f2cd2d0f8a05e6a6845ef23
= <=>
2022-02-18 18:00:04 -0300

    Ordem de Servico-API

---
+++ src/main/java/com/lhdrones/os/repositories/OSRepository.java
@@ -0,0 +1,11 @@
+package com.lhdrones.os.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lhdrones.os.domain.OS;
+
+@Repository
+public interface OSRepository extends JpaRepository<OS, Integer>{
+
+}
---
+++ src/test/java/com/lhdrones/os/OsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhdrones.os;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
